0938cfdd36f23241e4211291704a22c73068db48
Lingyu-Meng <147720003+Lingyu-Meng@users.noreply.github.com>
2023-12-29 17:09:56 +0000

    Update figure_of_key_gene_and_each_round.m

--- figure_of_key_gene_and_each_round.m
+++ figure_of_key_gene_and_each_round.m
@@ -1,11 +1,8 @@
 %% 2023/12/29 Lingyu
 % for 10 gene
 % There should only one file in result
-<<<<<<< Updated upstream
-
-=======
 % if 1,3 plot dotted line too hard to distinguish, change distinguish
->>>>>>> Stashed changes
+
 clc
 clear
 filepath=pwd;
@@ -13,90 +10,7 @@ file=dir([filepath,'/result/FAF*']);
 file_path = [filepath,'/result/',file.name];
 load(file_path)
 
-<<<<<<< Updated upstream
-%% prepare data
-XL(:,:) = mean(XMEAN_L, 3); % leader Initial_Investment, mean of runs 
-XF(:,:) = mean(XMEAN_F, 3); % follower Initial_Investment, mean of runs 
-
-LRL(:,:) = mean(LRMEAN_L, 3); % leader Learning_Rate, mean of runs
-LRF(:,:) = mean(LRMEAN_F, 3); % follower Learning_Rate, mean of runs
-
-BEL(:,:) = mean(BEMEAN_L, 3); % leader Last_Round_Behavior, mean of runs
-BEF(:,:) = mean(BEMEAN_F, 3); % follower Last_Round_Behavior, mean of runs
-
-
-k5  = [224,136,128]./255;
-k20  = [241,188,187]./255;
-k200  = [223,225,121]./255;
-
-highc=[0 0 0];
-lowc=[0.7 0.7 0.7];
-
-%%
-subplot(2,3,1)
-
-plot(1:skip:T,XL(:,1),'Color',k5,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,XF(:,1),'Color',k5,'LineWidth',4,'LineStyle','-.')
-hold on
-plot(1:skip:T,XL(:,2),'Color',k20,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,XF(:,2),'Color',k20,'LineWidth',4,'LineStyle','-.')
-hold on
-plot(1:skip:T,XL(:,3),'Color',k200,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,XF(:,3),'Color',k200,'LineWidth',4,'LineStyle','-.')
-hold on
-
-xlabel('Time','FontSize',24,'FontWeight','bold')
-ylabel('Initial contribution (x)','FontSize',30,'FontWeight','bold')
-ylim([0,10])
-set(gca,'box','off','linewidth',3,'FontSize',20)
-hold off
-
-subplot(2,3,2)
-
-plot(1:skip:T,LRL(:,1),'Color',k5,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,LRF(:,1),'Color',k5,'LineWidth',4,'LineStyle','-.')
-hold on
-plot(1:skip:T,LRL(:,2),'Color',k20,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,LRF(:,2),'Color',k20,'LineWidth',4,'LineStyle','-.')
-hold on
-plot(1:skip:T,LRL(:,3),'Color',k200,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,LRF(:,3),'Color',k200,'LineWidth',4,'LineStyle','-.')
-hold on
-
-xlabel('Time','FontSize',24,'FontWeight','bold')
-ylabel('Learning rate (u)','FontSize',30,'FontWeight','bold')
-ylim([0,1])
-set(gca,'box','off','linewidth',3,'FontSize',20)
-
-subplot(2,3,3)
-
-plot(1:skip:T,BEL(:,1),'Color',k5,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,BEF(:,1),'Color',k5,'LineWidth',4,'LineStyle','-.')
-hold on
-plot(1:skip:T,BEL(:,2),'Color',k20,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,BEF(:,2),'Color',k20,'LineWidth',4,'LineStyle','-.')
-hold on
-plot(1:skip:T,BEL(:,3),'Color',k200,'LineWidth',4,'LineStyle','-')
-hold on
-plot(1:skip:T,BEF(:,3),'Color',k200,'LineWidth',4,'LineStyle','-.')
-
-ylim([0,10])
-ylabel('Contribution in last round','FontSize',20,'FontWeight','bold')
-xlabel('Time','FontSize',24,'FontWeight','bold')
-set(gca,'box','off','linewidth',3,'FontSize',20)
-
-subplot(2,3,4)
-=======
 distinguish = 1; % affect 1,3 only now
->>>>>>> Stashed changes
 
 %% prepare data
 x = 1:skip:T;
@@ -211,11 +125,7 @@ hold off
 
 
 subplot(2,3,6)
-<<<<<<< Updated upstream
-plot(1:200, mean(INVMEN_L(end,1:200,3,:),4), 'LineWidth', 2)
-=======
 plot(1:200, mean(INVMEN_L(end,1:200,3,:),4), 'LineWidth', 2,'Color',k200,'LineStyle','-')
->>>>>>> Stashed changes
 hold on
 plot(1:200, mean(INVMEN_F(end,1:200,3,:),4), 'LineWidth', 2,'Color',k200,'LineStyle',':')
 hold on
